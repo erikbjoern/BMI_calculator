f5101d74f8ee60ac3fe6ffd5ada22d4c5f14658c
Erik Björn <erik.g.bjoern@gmail.com>
2020-03-26 17:47:32 +0100

    Creates HTML, CSS and JS for basic BMI calculator. Initializes cypress.

---
+++ assets/js/bmi_calculator.js
@@ -0,0 +1,21 @@
+const calculateMetricBMI = (weight, height) => {
+    let result
+    result = (weight / Math.pow((height / 100), 2)).toFixed(2)
+    return result
+}
+
+const getBMIClassification = (value) => {
+    if (value<18.5) {
+    return "underweight"
+    } else if (value < 25) {
+    return "normal weight"
+    } else if (value < 30) { 
+    return "overweight"
+    } else if (value < 35) {
+    return "obesity class 1"
+    } else if (value < 40) {
+    return "obesity class 2"
+    } else {
+    return "extreme obesity class 3"
+    } 
+}
